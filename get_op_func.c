8020b2c0d87b49fa708e0b70cdd4ddf35e396c56
matxa <matxa21@gmail.com>
2020-05-13 16:13:55 -0400

    fix codeclear

--- get_op_func.c
+++ get_op_func.c
@@ -31,18 +31,20 @@ char **parser(char *file_content)
  * Return: correct function to perform the operation
  */
 
-int (*get_op_func(char *s))(int, int)
-{
-    int i;
-
-	instruction_t ops[] = {};
-
-	i = 0;
-	while (ops[i].opcode != NULL)
-	{
-		if (*ops[i].opcode == *s && !(*(s + 1)))
-			return (ops[i].f);
-		i++;
-	}
-	return (NULL);
-}
+/**
+*int (*get_op_func(char *s))(int, int)
+*{
+*    int i;
+*
+*	instruction_t ops[] = {};
+*
+*	i = 0;
+*	while (ops[i].opcode != NULL)
+*	{
+*		if (*ops[i].opcode == *s && !(*(s + 1)))
+*			return (ops[i].f);
+*		i++;
+*	}
+*	return (NULL);
+*}
+*/
