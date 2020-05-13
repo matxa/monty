6c4598bf98b4dcbf9499410d36f538eedb6758fe
matxa <matxa21@gmail.com>
2020-05-13 19:45:49 -0400

    fix code

--- main.c
+++ main.c
@@ -11,7 +11,7 @@ int main(int argc, char **argv)
 {
 	char *file_name;
 	char *file_content;
-    char file_content_copy = [1024];
+    char file_content_copy[1024];
     unsigned int i;
     char **hold_tokens;
 	int c = 0;
