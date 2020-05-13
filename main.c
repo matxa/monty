fdfd8281bbceff6ad35e894b83bd39a5be6e4d88
matxa <matxa21@gmail.com>
2020-05-13 19:22:24 -0400

    fix code

--- main.c
+++ main.c
@@ -23,20 +23,6 @@ int main(int argc, char **argv)
 	file_name = argv[1];
 
 	file_content = open_read_file(file_name);
-	for (i = 0; i < file_size.st_size; i++)
-	{
-		printf("%c", file_content[i]);
-	}
-
-    token = strtok(file_content, " \n");
-	while (token != NULL)
-	{
-		b++;
-		token = strtok(NULL, " \n");
-        printf("%d: [%p] --- %s\n", i, token, token);
-	}
-    printf("\n");
-    printf("The while loop counted %d tokens\n", i);
 
 	return (1);
 }
