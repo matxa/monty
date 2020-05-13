2fb3a75a92466ca375dba640895656e8d63e50dc
matxa <matxa21@gmail.com>
2020-05-13 16:15:01 -0400

    fix codeclear

--- main.c
+++ main.c
@@ -11,7 +11,7 @@ int main(int argc, char **argv)
 {
 	char *arg_num = "Usage ./monty file_name.m";
 	char *file_name;
-	char *file_content;
+	char **file_content;
     char **tokens;
 	unsigned int i;
 
