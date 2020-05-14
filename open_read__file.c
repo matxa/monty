8115e5b3c24223785cb85819ec1fb8851ac1254d
matxa <matxa21@gmail.com>
2020-05-14 11:19:51 -0400

    clean file

--- open_read__file.c
+++ open_read__file.c
@@ -1,7 +1,7 @@
 #include "monty.h"
 
 /**
- * open_read_f - open and reads file
+ * read_f - open and reads file
  * @file_name: file name
  * Return: file file content
  */
@@ -9,27 +9,27 @@
 char *read_f(char *file_name)
 {
 	FILE *fp;
-    char *buffer;
-    long int buf_size;
-    struct stat st;
+	char *buffer;
+	long int buf_size;
+	struct stat st;
 
-    if(stat(file_name,&st)==0)
-         buf_size = st.st_size;
+	if (stat(file_name, &st) == 0)
+		buf_size = st.st_size;
 
-    buffer = malloc(sizeof(char) * buf_size);
-    if (!buffer)
-    {
-        perror("Error: malloc failed");
-        exit(EXIT_FAILURE);
-    }
+	buffer = malloc(sizeof(char) * buf_size);
+	if (!buffer)
+	{
+		perror("Error: malloc failed");
+		exit(EXIT_FAILURE);
+	}
 
-    fp = fopen(file_name, "r");
-    if (!fp)
-    {
-        fprintf(stderr,"Error: Can't open file %s\n", file_name);
-        exit(EXIT_FAILURE);
-    }
-    fread(buffer, sizeof(char), buf_size, fp);
+	fp = fopen(file_name, "r");
+	if (!fp)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", file_name);
+		exit(EXIT_FAILURE);
+	}
+	fread(buffer, sizeof(char), buf_size, fp);
 
 	fclose(fp);
 	return (buffer);
@@ -37,31 +37,30 @@ char *read_f(char *file_name)
 
 /**
  * parse_f - parse buffer
- * @buffer: array of chars
  * @file_name:file name
  * Return: file file content
  */
 
 char **parse_f(char *file_name)
 {
-    char **commands;
-    long int buf_size;
-    char *buffer;
-    struct stat st;
-    int i = 0;
+	char **commands;
+	long int buf_size;
+	char *buffer;
+	struct stat st;
+	int i = 0;
 
-    if(stat(file_name,&st)==0)
-        buf_size = st.st_size;
+	if (stat(file_name, &st) == 0)
+		buf_size = st.st_size;
 
-    buffer = malloc(sizeof(char) * buf_size);
-    commands = malloc(sizeof(char *) * buf_size);
-    buffer = read_f(file_name);
+	buffer = malloc(sizeof(char) * buf_size);
+	commands = malloc(sizeof(char *) * buf_size);
+	buffer = read_f(file_name);
 
-    commands[i] = strtok(buffer, " \n");
-    while (commands[i] != NULL)
-    {
-        i++;
-        commands[i] = strtok(NULL, " \n");
-    }
-    return (commands);
+	commands[i] = strtok(buffer, " \n");
+	while (commands[i] != NULL)
+	{
+		i++;
+		commands[i] = strtok(NULL, " \n");
+	}
+	return (commands);
 }
